3643f1e0e0e3a6ef8c2fc80e0d30571d5a6e46ac
timoknapp <mail@timoknapp.com>
2020-07-01 22:57:01 +0200

    [UPDATE] add urfave/cli dependency and play around

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"time"
+
+	"github.com/urfave/cli/v2"
 
 	"github.com/timoknapp/soundcloud-cli/pkg/download"
 	"github.com/timoknapp/soundcloud-cli/pkg/flag"
@@ -14,7 +17,50 @@ import (
 // BuildVersion contains SemVer of latest master merge
 var BuildVersion = "development"
 
+var app = cli.NewApp()
+
+func info() {
+	app.Name = "SoundCloud CLI"
+	app.Usage = "A simple CLI to interact with tracks on SoundCloud"
+	app.Version = "1.0.0"
+	app.Commands = commands()
+}
+
+func commands() []*cli.Command {
+	cmds := []*cli.Command{
+		{
+			Name:    "download",
+			Aliases: []string{"dl"},
+			Usage:   "Download a track",
+			Action: func(c *cli.Context) error {
+				dl := "download some stuff"
+				fmt.Println(dl)
+				return nil
+			},
+		},
+		{
+			Name:    "meta",
+			Aliases: []string{"m"},
+			Usage:   "Show metadata for a track",
+			Action: func(c *cli.Context) error {
+				m := "meta meta meta"
+				fmt.Println(m)
+				return nil
+			},
+		},
+	}
+	return cmds
+}
+
 func main() {
+	// info()
+	// commands()
+
+	// err := app.Run(os.Args)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
 	flags, err := flag.Read()
 	if err != nil {
 		log.Fatal(err)
@@ -24,7 +70,7 @@ func main() {
 		os.Exit(0)
 	}
 	if flags.TrackID == "" && flags.TrackURL == "" {
-		log.Fatal("trackID or trackPath must be provided")
+		log.Fatal("trackID or trackURL must be provided")
 	}
 	trackID := flags.TrackID
 	if flags.TrackURL != "" {
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/bogem/id3v2 v1.2.0
 	github.com/cheggaaa/pb/v3 v3.0.4
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	github.com/urfave/cli/v2 v2.2.0
 )
