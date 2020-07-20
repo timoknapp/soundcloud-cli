21baba57525eb5ed286f0ae302526ccc60e3aeac
timoknapp <mail@timoknapp.com>
2020-07-20 21:26:58 +0200

    [UPDATE] adapt print outs

--- pkg/sccli/sccli.go
+++ pkg/sccli/sccli.go
@@ -11,12 +11,12 @@ import (
 // PrintTable prints a fomatted table of tracks
 func PrintTable(tracks []soundcloud.Track) {
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"ID", "Artist", "Title", "URL", "Artwork-URL"})
-	table.SetHeaderColor(tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor})
+	table.SetHeader([]string{"ID", "Title", "URL"})
+	table.SetHeaderColor(tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor}, tablewriter.Colors{tablewriter.Bold, tablewriter.FgCyanColor})
 	table.SetBorder(false)
 	table.SetAlignment(tablewriter.ALIGN_LEFT)
 	for _, track := range tracks {
-		table.Append([]string{strconv.Itoa(track.ID), track.Artist.FullName, track.Title, track.PermalinkURL, track.ArtworkURL})
+		table.Append([]string{strconv.Itoa(track.ID), track.Title, track.PermalinkURL})
 	}
 	table.Render()
 }
