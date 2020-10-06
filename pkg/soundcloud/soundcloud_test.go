dee0fd007df156945cc0b6f78c62557a9430e1ae
timoknapp <mail@timoknapp.com>
2020-10-06 21:18:21 +0200

    test(init): add first simple test

---
+++ pkg/soundcloud/soundcloud_test.go
@@ -0,0 +1,19 @@
+package soundcloud
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetTrack(t *testing.T) {
+	trackID := "177318577"
+	trackTitle := "Elderbrook x Andhim - How Many Times"
+	quality := "mp3"
+	track, _ := GetTrack(trackID, quality)
+
+	trackIDint, _ := strconv.Atoi(trackID)
+	assert.Equal(t, trackIDint, track.ID)
+	assert.Equal(t, trackTitle, track.Title)
+}
